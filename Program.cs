954c9b90928e824312dcb29fc00a6cf468cb5b75
Petrik Diak <diak@petrik.hu>
2023-10-17 09:40:44 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statikus_Osztalyok
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+            // Veletlen v = new Veletlen();
+
+            Console.WriteLine(Veletlen.VelEgesz(2,60));
+			for (int i = 0; i < 10; i++) 
+			{ 
+            Console.WriteLine(Veletlen.VelKarakter('A','D'));
+			}
+		}
+
+
+
+	}
+}
---
+++ Veletlen.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statikus_Osztalyok
+{
+	internal static class Veletlen
+	{
+		// private string abc;
+		private static Random RND = new Random();
+		private static List<string> VEZETEKNEVEK=Feltolt("files/veznev.txt");
+		private static List<string> FERFI_KERESZTNEVEK = Feltolt("files/ferfikernev.txt");
+		private static List<string> NOI_KERESZTNEVEK = Feltolt("files/noikernev.txt");
+
+		private static List<string> Feltolt(string fajlnev)
+		{ 
+			var list = new List<string>();
+
+			using (StreamReader sr=new StreamReader(fajlnev))
+			{ 
+				while (sr.EndOfStream) 
+				{
+					var line = sr.ReadLine();
+					list.Add(line);
+				}
+			}
+
+			return list;
+		}
+
+		public static int VelEgesz(int min, int max)
+		{ 
+			return RND.Next(min, max+1);
+		}
+
+		public static char VelKarakter(char min, char max)
+		{
+			return (char)VelEgesz(min,max);
+		}
+
+	}
+}
